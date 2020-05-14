97d47af0f493c1aee2b2b672b34822695f5f0f13
ahmadridza <59468419+ahmadridza@users.noreply.github.com>
2020-05-15 01:32:36 +0800

    installed http package

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -7,8 +7,11 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -16,8 +19,7 @@ class _ChooseLocationState extends State<ChooseLocation> {
         title: Text('Choose a Location'),
         centerTitle: true,
         elevation: 0 ,),
-      body: Text('Change Location Screen'),
-
+      body: Text('Hello')
     );
   }
 }
