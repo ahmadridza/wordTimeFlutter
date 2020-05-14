8f251590d01b9c0a0582a4b3c6ce4b274d6ad82c
ahmadridza <59468419+ahmadridza@users.noreply.github.com>
2020-05-15 01:54:47 +0800

    requested API from jason placeholder in loading page

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 
 class Loading extends StatefulWidget {
@@ -10,7 +12,10 @@ class _LoadingState extends State<Loading> {
 
   void getData() async {
 
-  },
+    Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+    Map data = jsonDecode(response.body);
+    print(data['title ']);
+  }
   @override
   void initState() {
     super.initState();
